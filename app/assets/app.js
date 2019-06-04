2f403544675b0160ecee6f55bf7f94e99564c256
Antoni Nowinowski <antoninowinowski@hotmail.com>
2019-06-04 14:43:06 +0200

    frontend progress

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,11 +1,42 @@
 var app = angular.module('app', []);
 
 app.controller('mainController', function ($scope, $http) {
+
+    $scope.userRecommendation = [];
     $scope.getUserRecommendation = function () {
-        console.log($scope.username);
+        console.log($scope.user);
+
+        $http.post('/get_recommendation/user/' +  $scope.user, { test: $scope.user }).then(function (arr) {
+            console.log(arr);
+            $scope.userRecommendation = arr.data;
+        }).catch(function (err) {
+            console.log(err);
+        });
     }
 
+    $scope.titleRecommendation = [];
     $scope.getTitleRecommendation = function () {
         console.log($scope.title);
+
+        $http.post('/get_recommendation/title/' +  $scope.title, { test: $scope.title }).then(function (arr) {
+            console.log(arr);
+            $scope.titleRecommendation = arr.data;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+    $scope.animate = function () {
+        if ($scope.title && $scope.title.length) {
+            document.getElementById("title-field").classList.remove('empty');
+        } else {
+            document.getElementById("title-field").classList.add('empty');
+        }
+
+        if ($scope.user && $scope.user.length) {
+            document.getElementById("user-field").classList.remove('empty');
+        } else {
+            document.getElementById("user-field").classList.add('empty');
+        }
     }
 });
