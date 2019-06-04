3dc6a5d6a9d0111bf5be50593f340729f7a9a3b6
misztsu <adam.pawlowski.237@gmail.com>
2019-06-04 12:52:22 +0200

    add basic user scraper

---
+++ scrapers/user.py
@@ -0,0 +1,22 @@
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def getUserHtml(user):
+    res = requests.get('https://myanimelist.net/animelist/' + user + '?status=2)')
+    if res.status_code != 200:
+        raise RuntimeError('status code not 200')
+    return res.text
+
+def getUserData(user):
+    bs = BeautifulSoup(getUserHtml(user), 'html.parser')
+    tables = bs.find_all('table')
+    result = dict()
+    for table in tables:
+        j = json.loads(table.attrs['data-items'])
+        for record in j:
+            if record['score'] != 0:
+                result[record['anime_id']] = record['score']
+    return result
+
