98f7924fe3d1ae878fdddc4bf45599d3c6986c01
misztsu <adam.pawlowski.237@gmail.com>
2019-06-04 14:11:33 +0200

    working id to name

--- scrapers/user.py
+++ scrapers/user.py
@@ -3,14 +3,14 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-def getUserHtml(user):
+def _getUserHtml(user):
     res = requests.get('https://myanimelist.net/animelist/' + user + '?status=2)')
     if res.status_code != 200:
         raise RuntimeError('status code not 200')
     return res.text
 
 def getUserData(user):
-    bs = BeautifulSoup(getUserHtml(user), 'html.parser')
+    bs = BeautifulSoup(_getUserHtml(user), 'html.parser')
     tables = bs.find_all('table')
     result = dict()
     for table in tables:
